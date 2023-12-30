bf54312fdc4ecb10f6f4df500824e45c2c3d3d11
prateek <prateeksingh8841@gmail.com>
2023-12-29 22:02:37 -0700

    feat: register page

---
+++ src/utils/styles/index.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const InputField = styled.input`
+  outline: none;
+  border: none;
+  color: black;
+  font-size: 18px;
+  width: 100%;
+  box-sizing: border-box;
+  background-color: inherit;
+  color: #fff;
+  padding: 0;
+`;
+
+export const InputContainer = styled.div`
+  background-color: #131313;
+  color: #fff;
+  width: 100%;
+  padding: 12px 16px;
+  box-sizing: border-box;
+  border-radius: 10px;
+`;
+
+export const InputLabel = styled.label`
+  display: block;
+  color: #8f8f8f;
+  font-size: 14px;
+  margin: 4px 0;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  color: #fff;
+  background-color: #2b09ff;
+  border: none;
+  outline: none;
+  font-size: 16px;
+  border-radius: 10px;
+  padding: 25px;
+`;
+
+export const Page = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
